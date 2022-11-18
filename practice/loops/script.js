85fdfe668392760d2ceeefb00a85c49746155c62
nishi1305 <gnishi133@gmail.com>
2022-11-18 21:37:06 +0530

    commit

---
+++ practice/loops/script.js
@@ -0,0 +1,104 @@
+
+//wap to print all natural numbers in reverse.//
+
+// let n=20;
+// for(i=n;i>=1;i--)
+// {
+//     console.log(i);
+// }
+
+
+
+
+//wap to print all even numbers between 1 to 100.//
+
+// let n=1;
+// for(i=n;i<=100;i++)
+// if(i%2==0)
+// {
+//     console.log(i);
+// }
+
+
+
+
+//wap to print all odd numbers between 1 to 100.//
+
+// let n=1;
+// for(i=n;i<=100;i++)
+// if(i%2!==0)
+// {
+//     console.log(i);
+// }
+
+
+
+
+//wap to display the sum of n natural numbers.//
+
+// let n=3,sum=0;
+// for(i=1;i<=n;i++)
+// {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+
+
+//wap to display the sum of even numbers.//
+
+// let n=4,sum=0;
+// for(i=1;i<=n;i++)
+// if(i%2==0)
+// {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+
+
+
+//wap to display the sum of odd numbers.//
+
+// let n=4,sum=0;
+// for(i=1;i<=n;i++)
+// if(i%2!==0)
+// {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+
+
+// wap to print multiplication table of any number.//
+
+// let n=5,M=0;
+// for(i=1;i<=10;i++)
+// {
+//     M = i * n;
+//     console.log(M);
+
+// }
+
+
+
+
+//wap to count no. of digits in a number.//
+
+// let num="123456";
+// let strlength=num.length;
+// console.log(strlength);
+
+
+
+
+
+
+
+
+
+
+
