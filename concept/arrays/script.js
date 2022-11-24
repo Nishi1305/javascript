d9a85d6e25f0d1d71237cbe77ec7dbc55ef11f09
nishi1305 <gnishi133@gmail.com>
2022-11-24 13:02:43 +0530

    commit

---
+++ concept/arrays/script.js
@@ -0,0 +1,59 @@
+// let cars=["mahindra","tata","audi","toyoto","porche","tesla"];
+// let nishicar=cars[1];
+// console.log(nishicar) ;
+
+
+// let num=[12,13,4,5,98,100,52]
+// let number=num[2];
+// console.log(number);
+// let number=num[5];
+// console.log(number);
+// let number=num[6];
+// console.log(number);
+
+
+
+
+//changing an array element//
+// let cars=["mahindra","tata","audi","toyoto","porche","tesla"];
+// console.log(cars);
+// cars[4]="BMW";
+// console.log(cars);
+
+
+
+//js store multiple type of data//
+// let num=[13,"nishi",-14,0.45,true,undefined]
+// console.log(num ,typeof num);
+// console.log(Array.isArray(num));
+
+
+
+// let name="nishi gupta";
+// console.log(Array.isArray(name));             //for type of data
+
+
+//to find the length//
+// let cars=["mahindra","tata","audi","toyoto","porche","tesla"];
+// let str=cars.length;
+// console.log(str);
+
+
+
+//to print the data without using index number//
+// let fruits=["apple","mango","banana","grapes"];
+// let last=fruits[fruits.length -1];
+// console.log(last);
+
+
+
+//looping  of array//
+// let cars=["mahindra","tata","audi","toyoto","porche","tesla"];
+// for(let i=0; i<cars.length; i++)
+// {
+//     console.log(cars[i]);
+// }
+
+
+
+
--- concept/strings/script.js
+++ concept/strings/script.js
@@ -1,7 +1,11 @@
+//to find the lenght of string//
 // let name="string";
 // let strlength=name.length;
 // console.log(strlength)
 
+
+//********** Escape sequences************//
+//backslash character//
 // let name=" my name is\"nishi\"";
 // console.log(name);
 
@@ -11,7 +15,7 @@
 // console.log(strlength);
 
 
-
+//add two string variable//
 // let firstname="nishi";
 // let lastname="gupta"
 // let fullname=firstname + lastname;
@@ -25,14 +29,20 @@
 
 
 
-
+//string as object//
 // let x="nishi";
 // let y=new String("nishi");
 // console.log(typeof y);
 // console.log(typeof x);
 
 
-// let x=`nishi`;               //templete literals
+
+
+
+//*****************templete literals******************//
+
+//Back tick//
+// let x=`nishi`;              
 // let y="nishi";
 // console.log(typeof y);
 // console.log(typeof x);
@@ -52,7 +62,7 @@
 // console.log(text);
 
 
-
+//**************String Interpolation*************//
 // let fname="nishi";
 // let lname="gupta"
 // console.log("my name is"+ fname+ lname )
@@ -60,10 +70,90 @@
 
 
 
-
+//expression substitution//
 // let a=5,b=2;
 // let product=`axb=${a*b}`;
-// console.log(product)
+// console.log(product);
+
+
+
+
+
+
+//**************************STRING METHODS********************//
+
+// let text ="the quick brown fox jump over the lazy dog";
+// let capital=text.toUpperCase();
+// console.log(capital);
+
+
+
+
+// let name="NISHI GUPTA";
+// let lower=name.toLowerCase();
+// console.log(lower);
+
+
+
+
+
+// let=Info="My Name is";
+// let=NewInfo=Info.concat( ' nishi',' gupta');
+// console.log(NewInfo);
+
+
+
+
+
+
+// let text ="the quick brown fox jump over the lazy dog";
+// console.log(text[7]);
+// console.log(text.indexOf("fox"));
+// console.log(text.indexOf("ox"));
+// console.log(text.indexOf("xo"));
+
+
+// let text ="the quick brown fox jump over the fox lazy dog";
+// console.log(text.lastIndexOf("fox"));
+
+
+
+// let text ="the quick brown fox jump over the lazy dog";
+// console.log(text.charAt(12));
+
+// console.log(text.endsWith('lazy'));      //this return boolean value//
+// console.log(text.endsWith('dog'));
+
+// console.log(text.includes('over'));
+
+
+/////////////extracting string//////////////
+// let text ="the quick brown fox jump over the lazy dog";
+
+// let result=text.slice(0,3);
+// let result=text.slice(0,9);
+// let result=text.slice(0);
+// let result=text.slice(-8,-2);
+// let result=text.slice(-6);
+// console.log(result);
+
+
+
+
+/////////////////sub string//////////////////
+// let text ="the quick brown fox jump over the lazy dog";
+// let result=text.substring(7,12);
+// console.log(result)
+
+
+///////////replace method//////////////
+// let text ="the quick brown fox jump over the lazy dog";
+// let result=text.replace('dog','turtle');
+// console.log(result);
+
+
+
+
 
 
 
