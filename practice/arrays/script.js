e6cc6cbc9e4a967da4555a33242aab5c399cc0a6
nishi1305 <gnishi133@gmail.com>
2022-11-25 16:50:57 +0530

    commit

---
+++ practice/arrays/script.js
@@ -0,0 +1,130 @@
+
+//right angle triangle//
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++)
+//  {
+//     for (let j = 0; j < n - i; j++) {
+//     string += " ";
+//   }
+
+//   for (let k = 0; k < i; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+//Left Triangle Pattern //
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++)
+//  {
+//     for (let j = 0; j < i; j++) {
+//     string += "*";
+//   }
+
+//   for (let k = 0; k < n-i ; k++) {
+//     string += " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+//downward left triangle//
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++)
+//  {
+//     for (let j = 0; j <=n-i ; j++) {
+//     string += "*";
+//   }
+
+//   for (let k = 1; k < i ; k++) {
+//     string += " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+// star square pattern//
+let n=5;
+let string=" ";
+for (let i=0 ; i<n ;i++)
+{
+    for(let j=0 ; j<n ;j++)
+    {
+        string+="*";
+    }
+  string +="\n"
+}
+console.log(string);
+
+
+
+
+
+//hollow square pqttern//
+// let n=5;
+// let string=" ";
+// for(let i=0;i<n;i++)
+// {
+//     for(j=0; j<n; j++)
+//     {
+//         if(i===0 || i=== n-1)
+//         {
+//             string +="*";
+//         }
+//         else{
+//             if(j===0 || j=== n-1 )
+//             {
+//                 string += " ";
+//             }
+//             else{
+//                 string += " ";
+//             }
+//         }
+//     }
+//     string += "\n";
+
+// }
+// console.log(string);
+
+
+
+
+
+// //hollow triangle pattern//
+// let n=6;
+// let string=" ";
+// for(let i=1; i<=n; i++)
+// {
+//     for(let j=0; j<i ;j++)
+//     {
+//         if(i===n){
+//             string +="*";
+
+//         }
+//         else{
+//             if ( j===0 || j==i -1){
+//                 string +="*";
+//             }
+//             else{
+//                 string+=" "
+//             }
+//         }
+//     }
+//     string +="\n"
+// }
+// console.log(string);
+
+
+
