bb25e66c9fbb83af38827c39f62cac37fee9a523
nishi1305 <gnishi133@gmail.com>
2022-11-25 16:55:31 +0530

    commit

---
+++ concept/functions/script.js
@@ -0,0 +1,35 @@
+// function addtwonum(a,b)   //function definition//
+// {
+//     let c=a+b;
+//     console.log(c);
+// }
+
+// addtwonum(1000,5000);           //function call//
+
+
+// function addtwonum(a,b)  
+// {
+//     let c=a-b;
+//     console.log(c);
+// }
+
+// addtwonum(1000,500);    
+
+
+// function addtwonum(a,b)  
+// {
+//     let c=a*b;
+//     console.log(c);
+// }
+
+// addtwonum(100,50);    
+
+
+
+function addtwonum(a,b)  
+{
+    let c=a**b;
+    console.log(c);
+}
+
+addtwonum(10,3);    
